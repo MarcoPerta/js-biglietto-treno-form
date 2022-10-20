cde627fc8dfd3cd6fe19d691086cd905dc2876b9
MarcoPerta <perta.marco@gmail.com>
2022-10-20 15:34:37 +0200

    prime dichiarazioni variabili js

---
+++ main.js
@@ -0,0 +1,6 @@
+let NameBuyer = document.getElementById('NameAndSurname').value;
+console.log(NameBuyer)
+let Km = document.getElementById('HowmanyKm').value;
+console.log(Km)
+let Age = document.getElementById('Age').value;
+console.log(Age)
